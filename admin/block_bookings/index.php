def6dbe4dbae2eb0cb3a38de132cf336f662046c
Bharath1702 <bharathac7@gmail.com>
2024-05-07 22:04:11 +0530

    latest

--- admin/block_bookings/index.php
+++ admin/block_bookings/index.php
@@ -1,3 +1,20 @@
+<?php
+session_start();
+if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
+    header("Location: index.html");
+    exit;
+}
+
+// Logout functionality
+if (isset($_GET['logout']) && $_GET['logout'] == 'true') {
+    session_unset(); // Unset all session variables
+    session_destroy(); // Destroy the session
+    header("Location: index.html"); // Redirect to login page
+    exit;
+
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
