db382c33f05d66c9b94b13839cfbaff362ead8f0
Bharath1702 <bharathac7@gmail.com>
2024-05-11 14:58:30 +0530

    latest

--- admin/block_bookings/db.php
+++ admin/block_bookings/db.php
@@ -1,11 +1,11 @@
 <?php
+//$servername = "localhost";
+//$username = "root";
+//$password = "";
+//$database = "bookings";
 $servername = "localhost";
-$username = "root";
-$password = "";
-$database = "bookings";
-// $servername = "localhost";
-// $username = "escapemgm_gateway";
-// $password = "Escape@2024";
-// $database = "escapemgm_gateway";
+$username = "escapemgm_gateway";
+$password = "Escape@2024";
+$database = "escapemgm_gateway";
 $conn = new mysqli($servername, $username, $password, $database);
 ?>
