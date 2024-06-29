89bc5795a638df9a9d809e29f621d2c7519e8220
Bharath1702 <bharathac7@gmail.com>
2024-06-29 07:53:08 +0530

    new

--- games/ruins_of_hampi/pay.php
+++ games/ruins_of_hampi/pay.php
@@ -2,8 +2,6 @@
 require './utils/config.php';
 require './utils/common.php';
 
-
-
 // Check API status and set variables accordingly
 if (API_STATUS == 'LIVE') {
     $merchantid = MERCHANTIDLIVE;
@@ -27,6 +25,8 @@ curl_close($ch);
 if (isset($_POST['name'], $_POST['email'], $_POST['date'], $_POST['timeslot'], $_POST['mobile'], $_POST['qty'], $_POST['amount'])) {
     session_start();
     session_regenerate_id();
+
+    // Function to generate a random value
     function generateRandomValue()
     {
         return bin2hex(random_bytes(16)); // Generate a 32-character hexadecimal random string
@@ -42,8 +42,8 @@ if (isset($_POST['name'], $_POST['email'], $_POST['date'], $_POST['timeslot'], $
     } else {
         // Retrieve the random value from the cookie
         $randomValue = generateRandomValue();
-        setcookie('randomValue', $randomValue, time() + 6000, "/"); 
     }
+
     // Assign POST data to session variables
     $_SESSION['name'] = $_POST['name'];
     $_SESSION['email'] = $_POST['email'];
@@ -52,8 +52,9 @@ if (isset($_POST['name'], $_POST['email'], $_POST['date'], $_POST['timeslot'], $
     $_SESSION['mobile'] = $_POST['mobile'];
     $_SESSION['qty'] = $_POST['qty'];
     $_SESSION['amount'] = $_POST['amount'];
-    $paymentstatus = 'PENDING';
     $game = "RUINS OF HAMPI";
+    $status = "PENDING";
+    // Insert data into the cart table
     if (isset($_COOKIE['randomValue'])) {
         // Database connection
         require './utils/db.php';
@@ -76,17 +77,15 @@ if (isset($_POST['name'], $_POST['email'], $_POST['date'], $_POST['timeslot'], $
     }
 
     // Prepare the SQL statement to insert data into the cart table
-    $insertStmt = $conn->prepare("INSERT INTO cart (cookie_id, name, email, mobile,game, date, no_of_players, timeslot_id,amount,payment_status) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ? , ?)");
-    $insertStmt->bind_param("ssssssisis", $randomValue, $_SESSION['name'], $_SESSION['email'], $_SESSION['mobile'],$game, $_SESSION['date'], $_SESSION['qty'], $_SESSION['timeslot'], $_SESSION['amount'],$paymentstatus);
+    $insertStmt = $conn->prepare("INSERT INTO cart (cookie_id, name, email, mobile,game, date, no_of_players, timeslot_id,amount, payment_status) VALUES (?, ?, ?, ?, ?, ?, ?, ? , ? , ?)");
+    $insertStmt->bind_param("ssssssisis", $randomValue, $_SESSION['name'], $_SESSION['email'], $_SESSION['mobile'],$game, $_SESSION['date'], $_SESSION['qty'], $_SESSION['timeslot'], $_SESSION['amount'],$status);
 
     // Execute the statement and check for success
     $insertStmt->execute();
     // Close the statement and the database connection
     $insertStmt->close();
     $conn->close();
-} 
-    // Insert data into the cart table
-    }else {
+} }else {
     echo "<script>alert('Please try clearing the browser cache and try again')</script>";
 }
 
